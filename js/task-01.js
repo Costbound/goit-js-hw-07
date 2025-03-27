471028297b156de3034eb7f484a46d465c1ad87b
Dmytro Popovskyi <popovskiy96@gmail.com>
2025-03-27 01:33:43 +0100

    done

---
+++ js/task-01.js
@@ -0,0 +1,11 @@
+"use strict";
+const categories = document.querySelectorAll(".item");
+console.log(`Categories quantity: ${categories.length}`);
+
+categories.forEach(category => {
+    const title = category.querySelector(`h2`);
+    const items = category.querySelectorAll(`ul li`);
+    console.log(`Category: ${title.textContent}`);
+    console.log(`Items: ${items.length}`);
+})       
+
